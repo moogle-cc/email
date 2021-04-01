496b750918c9464a3d503842468261399b43b7ef
Neelesh Thakur <nt00605@gmail.com>
2021-04-01 14:57:56 +0530

    fixed variable names and removed host name

--- src/worker.js
+++ src/worker.js
@@ -1,13 +1,13 @@
 export const fetchList = async (data) => { // eslint-disable-line no-restricted-globals
     let {fqdn, authDetails, EMAILS_LIST_URL, emailSet} = data;
-    const newEmailFound = true;
+    const NEW_EMAIL_FOUND = true;
     await fetch(`${EMAILS_LIST_URL}?domain=${fqdn}&folderpath=/email`,{
       headers: {'Authorization':authDetails.id_token},
     }).then((response)=> response.json())
     .then(async (response) => { 
       if(response.Contents.length > 0 && emailSet && response.Contents[0].Key !== emailSet[0].Key){ 
         console.log("new email found")
-          postMessage({newEmailFound});
+          postMessage({NEW_EMAIL_FOUND});
         };
     })
 };
