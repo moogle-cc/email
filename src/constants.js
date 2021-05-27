c9b75ef6bda56b2da3be9f70195c1c6480df0737
Sai Ramachandran <sai@ramachandr.in>
2021-05-27 07:23:52 +0530

    #77 porting bugfix to main branch and reverting changes to constants file

--- src/constants.js
+++ src/constants.js
@@ -10,10 +10,10 @@ export const COMMENT_POST_URL = `${API_GW_URL}/email/comments`;
 export const DEFAULT_FQDN = HOST.startsWith('localhost') ? 'ramachandr.in' : HOST;
 export const LOGIN_REDIRECT_URL = `${ORIGIN}${PATHNAME}`;
 // export const LOGOUT_REDIRECT_URL = `${ORIGIN}${PATHNAME}`;
-export const COGNITO_URL = 'https://zeer0.auth.ap-south-1.amazoncognito.com/';
-export const CLIENT_ID = '7iia3e63t239ul2el9vrqfkb3v';
+export const COGNITO_URL = 'https://moogle.auth.ap-south-1.amazoncognito.com/';
+export const CLIENT_ID = '365ebnulu59p2fkp1m6dl0v6gd';
 export const RESPONSE_TYPE = 'token';
 export const SCOPE = 'email+openid';
 export const NEW_EMAIL_CHECKOUT_TIME = 300000;
 export const COGNITO_LOGIN_URL = `${COGNITO_URL}/login?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&redirect_uri=${LOGIN_REDIRECT_URL}`;
-export const EMAIL_FOLDERPATH_QP_STRING = ``; //folderpath=/email
+export const EMAIL_FOLDERPATH_QP_STRING = `folderpath=/email`;
